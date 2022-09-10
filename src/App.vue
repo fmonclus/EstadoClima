9736fc665a79abfb82b9351096ee560a8497d67b
Fabian Spabentti Monclus <fmonclus@tnmprogramming.com.ar>
2022-09-10 20:29:28 -0300

    update bootstrap

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import HelloWorld from "./components/Inicio.vue";
 </script>
 
 <template>
-  <nav class="navbar navbar-default navbar-inverse" role="navigation">
+  <!-- <nav class="navbar navbar-default navbar-inverse" role="navigation">
     <div class="container-fluid">
       <div class="navbar-header">
         <button
@@ -21,9 +21,7 @@ import HelloWorld from "./components/Inicio.vue";
           <span class="icon-bar"></span>
         </button>
         <a class="navbar-brand" href="#">Estado del clima</a>
-      </div>
-
-      <!-- Collect the nav links, forms, and other content for toggling -->
+      </div>      
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
           <li>
@@ -35,7 +33,35 @@ import HelloWorld from "./components/Inicio.vue";
         </ul>
       </div>
     </div>
+  </nav> -->
+  
+  <nav class="navbar navbar-default navbar-inverse" role="navigation">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Estado Clima</a>
+      </div>
+      
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+        <li>
+           <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li>
+            <router-link class="nav-link" to="/EstadoClima">Pronosticos</router-link>
+          </li>          
+        </ul>
+      </div>
+    </div>
   </nav>
+
+
+
   <RouterView />
 </template>
 
--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -2,30 +2,50 @@
 </script>
 
 <template>
-  <h3><strong>Bienvenidos a Estado del clima</strong></h3>
-  <h4>
-    <p class="descripcion">Esta es una aplicacion para poner en practicas conceptos de Vue.JS</p>
-    <p class="descripcion">Utilizando la API de weatherDB </p>
-    <p class="descripcion"><strong>Documentacion API Clima: </strong><a href="https://weatherdbi.herokuapp.com/documentation/v1" target="_blank">weatherdbi.herokuapp.com</a></p>
-    <p class="descripcion"><strong>Wallpaper: </strong><a href="https://www.wallpaperup.com/" target="_blank">www.wallpaperup.com</a></p>
-  </h4>
+  <div class="margen">
+    <h3><strong>Bienvenidos a Estado del clima</strong></h3>
+    <h4>
+      <p class="descripcion">
+        Esta es una aplicacion para poner en practicas conceptos de Vue.JS
+      </p>
+      <p class="descripcion">Utilizando la API de weatherDB</p>
+      <p class="descripcion">
+        <strong>Documentacion API Clima: </strong
+        ><a
+          href="https://weatherdbi.herokuapp.com/documentation/v1"
+          target="_blank"
+          >weatherdbi.herokuapp.com</a
+        >
+      </p>
+      <p class="descripcion">
+        <strong>Wallpaper: </strong
+        ><a href="https://www.wallpaperup.com/" target="_blank"
+          >www.wallpaperup.com</a
+        >
+      </p>
+    </h4>
+  </div>
 </template>
 
 <style scoped>
-  h3 {    
-    color: #E8AF2E;
-    padding-bottom: 10px;
-  }
+h3 {
+  color: #e8af2e;
+  padding-bottom: 10px;
+}
 
-  a {
-    color: #E8AF2E;    
-  }
+a {
+  color: #e8af2e;
+}
 
-  a:hover {
+a:hover {
   text-decoration: underline;
 }
 
-  .descripcion {
-    color: #F7E4B8;
-  }
+.descripcion {
+  color: #f7e4b8;
+}
+
+.margen {
+  padding-left: 15px;
+}
 </style>
